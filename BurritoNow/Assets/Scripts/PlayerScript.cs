026e4419a68bead5a148b81d21094f0621830516
souldzin <mmapslaughter@gmail.com>
2017-09-02 11:25:59 -0500

    starting unity project and collision

---
+++ BurritoNow/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+    public int speed = 1;
+    public Vector2 direction = Vector2.up;
+
+    private Vector3 _initialPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _initialPosition = this.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 translate = direction * speed * Time.deltaTime;
+
+        this.transform.Translate(translate);
+	}
+
+    void Die ()
+    {
+        Debug.Log("You died!");
+        this.transform.SetPositionAndRotation(_initialPosition, Quaternion.identity);
+    }
+}
