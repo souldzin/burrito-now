b1a6f8628fb811d65dab50b4dbea87ffa4a11b5a
souldzin <mmapslaughter@gmail.com>
2017-09-02 12:34:56 -0500

    imported sprites + starting the shoot

---
+++ BurritoNow/Assets/Scripts/TruckController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckController : MonoBehaviour {
+    private TruckScript _script;
+
+	// Use this for initialization
+	void Start () {
+        _script = GetComponent<TruckScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _script.ShootBurrito();
+        }        
+	}
+}
---
+++ BurritoNow/Assets/Scripts/TruckScript.cs
@@ -0,0 +1,58 @@
+﻿using Spine.Unity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckScript : MonoBehaviour {
+    const string ANIMATION_FIRE = "fire";
+
+    public float speed = 1.0f;
+    public Vector2 direction = Vector2.up;
+    public GameObject burritoPrefab;
+
+    private Vector3 _initialPosition;
+    private SkeletonAnimation _skeletonAnimation;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _initialPosition = this.transform.position;
+        _skeletonAnimation = this.GetComponent<SkeletonAnimation>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 translate = direction * speed * Time.deltaTime;
+
+        this.transform.Translate(translate);
+	}
+
+    public void ShootBurrito()
+    {
+        Debug.Log("You burritoed!");
+        PlayShootAnimation();
+        CreateBurrito();
+    }
+
+    public void Die ()
+    {
+        Debug.Log("You died!");
+        this.transform.SetPositionAndRotation(_initialPosition, Quaternion.identity);
+    }
+
+    // ShootBurrito helpers
+    // ----------------
+
+    private void CreateBurrito()
+    {
+        var position = transform.position;
+        var rotation = transform.rotation;
+        var burrito = Instantiate(burritoPrefab, position, rotation);
+    }
+
+    private void PlayShootAnimation()
+    {
+        _skeletonAnimation.state.AddAnimation(1, ANIMATION_FIRE, false, 0);
+    }
+}
